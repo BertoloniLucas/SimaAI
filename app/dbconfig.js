3c7deef293f1ea8961ee0dcc1e62f27fd13c08d0
Bertoloni Lucas <lucasbertoloni5@gmail.com>
2024-08-12 09:36:32 -0300

    fix(config): Fix client object and export

--- app/dbconfig.js
+++ app/dbconfig.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv/config'
+import pkg from 'pg';
+const { Client } = pkg;
 
-export const config = {
+export const client = new Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST, 
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD, 
     port: 5432,
-    ssl: true,
-};
+    ssl : true, 
+});

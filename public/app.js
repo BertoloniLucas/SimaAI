c7a5e28a4a87234dc54200921a41cdbab9c5e4df
Bertoloni Lucas <lucasbertoloni5@gmail.com>
2024-08-15 11:37:27 -0300

    trial

--- public/app.js
+++ public/app.js
@@ -2,3 +2,8 @@ const nombre = document.getElementById('name')
 const surname = document.getElementById('surname')
 const button = document.getElementById('btn1')
 
+button.addEventListener("click", ()=>{
+    fetch("https://proyecto2024-opr7j2xpe-lucas-bertolonis-projects.vercel.app/users")
+    .then(res => res.json())
+    .then(data => alert(data))
+})

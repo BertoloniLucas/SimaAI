9609edc750eec7eac70bcb710797207a8bfa5126
Bertoloni Lucas <lucasbertoloni5@gmail.com>
2024-08-20 22:02:49 -0300

    Add prisma CRUD

--- app/index.js
+++ app/index.js
@@ -14,11 +14,15 @@ const port = 3002
 
 //------------------------------//
 
-await prisma.$connect()
+await prisma.$connect() // Conexión Inicial
 
 app.listen(port, () => {
     console.log(`Sima listening on port`, port)
 })
+
+
+
+//--------------GET Routes-------------------//
 
 
 app.get("/", async (_, res) => {
@@ -47,3 +51,57 @@ app.get("/name/:name", async (req, res) => {
     
     users.length > 0 ? res.send(users) : res.status(404).send("Not Found")
 })
+
+
+//------------End GET Routes----------------//
+
+//------------POST Routes-------------------//
+
+app.post("/register", async (req, res) => {
+    const {userName, userSurname} = req.body
+    const addUser = await prisma.users.create({
+        data: {
+            name: userName,
+            surname: userSurname
+        }
+    })
+    
+    res.status(201).send("Creado OK!")
+})
+
+
+//-----------End POST Routes----------------//
+
+//------------PUT Routes--------------------//
+
+app.put("/editUser/:id", async (req, res) => {
+    const {id} = req.params
+    const {userName, userSurname} = req.body
+    const editUser = await prisma.users.update({
+        where: {id: parseInt(id)}, 
+        data: {
+            name: userName,
+            surname: userSurname
+        }
+    })
+
+    editUser? res.status(201).send("Actualizado con exito!") : res.send(404).send("No se ha encontrado al usuario")
+})
+
+
+//---------End PUT Routes---------------------//
+
+//-----------DELETE Routes--------------------//
+
+
+app.delete("/deleteUser/:id", async (req, res) => {
+    const {id} = req.params
+    const deleteUser = await prisma.users.delete({
+        where: {id: parseInt(id)}
+    })
+
+    deleteUser? res.status(200).send("Eliminado exitosamente") : res.send(404).send("Not found")
+})
+
+
+//----------End DELETE Routes-----------------//
